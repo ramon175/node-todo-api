60d9b67b3f075bdb0211f776779deda78d6e7c56
Ramon Castan Sutto <ramon.castan@ramons-mbp.br.ibm.com>
2018-09-25 16:35:26 -0300

    update

--- playground/mongodb.update.js
+++ playground/mongodb.update.js
@@ -8,7 +8,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) => {
     const db = client.db('TodoApp');
 
     //findOneAndUpdate
-    
+    // db.collection("Todos").findOneAndUpdate({
+    //     _id: new ObjectID("5baa73c4574242e64e98e5d5")
+    // },{
+    //     $set:{
+    //         completed:true
+    //     }
+    // }, { returnOriginal: false }).then(result => {
+    //     console.log(result);
+    // })
+
+    db.collection("Users").findOneAndUpdate({
+        _id: new ObjectID("5baa8ce0c41c6f0bf8d72121")
+    },{
+        $set:{
+            name:"Vrau"
+        },
+        $inc:{
+            age: -2
+        }
+    }, { returnOriginal: false }).then(result => {
+        console.log(result);
+    })
 
     client.close();
     
