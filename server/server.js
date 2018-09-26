ed54fc3afc7d1fdc989861d679b5c95996e2fe50
Ramon Castan Sutto <ramon.castan@Ramons-MBP.home>
2018-09-26 15:09:38 -0300

    test dependencies

--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 
 let {Todo} = require('./models/todo');
 let {USer} = require('./models/user');
 
+let PORT = 3000;
+let app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/todos', (req,res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => res.status(400).json({err}))
+    
+});
+
+
+app.listen(PORT, () => {
+    console.log("Started on port " + PORT);
+});
 
